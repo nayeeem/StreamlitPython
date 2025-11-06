177d7dea1f845809c61439d632f5fa6e5bb05af8
Naim Prodhan <naimul.prodhan81@hotmail.com>
2025-11-06 18:55:10 +0600

    Refactor chai selection to use a separate array for options

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ st.title("My First Chai Streamlit App")
 st.subheader("Streamlit Demo by Naim")
 st.text("CHAI IS THE BEST TEA!")
 st.write("Welcome to our first ineractive app!")
-
-chai = st.selectbox("Fev. Chai:", ["Masala Chai", "Ginger Chai", "Lemon Chai"])
+arrchai = ["Masala Chai", "Ginger Chai", "Lemon Chai"]
+chai = st.selectbox("Fev. Chai:", arrchai)
 st.button("Click Me!")
 
 st.write(f"You choose {chai}. Excellient choice!")
 st.success("Enjoy your chai!")
-st.balloons()
-st.snow()
-st.snow()
